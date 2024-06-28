c9b5bfad11dcd1b7812a72a82250c6c07de09109
SoullessMindful <slawomir.kusinski@polsl.pl>
2024-06-28 19:49:52 +0200

    Day 5: Rust: For the sake of transparency: my first - not that clever - attempt at part two. Had it not taken over 10 GB of RAM in a matter of seconds, it might have actually worked.

---
+++ day/5/rust/day-5-part-2.rs
@@ -0,0 +1,111 @@
+use std::env::args;
+use std::fs::read_to_string;
+use std::iter::zip;
+
+fn main() {
+    let clargs: Vec<String> = args().collect();
+
+    let filepath = &clargs[1];
+
+    let lines: Vec<String> = read_to_string(filepath) 
+        .unwrap()
+        .split("\n\n")
+        .map(String::from)
+        .collect();
+
+    let seed_ranges = extract_seeds(lines[0].clone());
+    let seeds = extract_seeds_from_ranges(seed_ranges);
+
+    let seed_to_soil = extract_rules(lines[1].clone());
+    let soil_to_fertilizer = extract_rules(lines[2].clone());
+    let fertilizer_to_water = extract_rules(lines[3].clone());
+    let water_to_light = extract_rules(lines[4].clone());
+    let light_to_temperature = extract_rules(lines[5].clone());
+    let temperature_to_humidity = extract_rules(lines[6].clone());
+    let humidity_to_location = extract_rules(lines[7].clone());
+
+    let locations: Vec<i64> = seeds.into_iter()
+        .map(|s| apply_rules(s, seed_to_soil.clone()))
+        .map(|s| apply_rules(s, soil_to_fertilizer.clone()))
+        .map(|s| apply_rules(s, fertilizer_to_water.clone()))
+        .map(|s| apply_rules(s, water_to_light.clone()))
+        .map(|s| apply_rules(s, light_to_temperature.clone()))
+        .map(|s| apply_rules(s, temperature_to_humidity.clone()))
+        .map(|s| apply_rules(s, humidity_to_location.clone())).collect::<Vec<i64>>();
+
+    println!("{:?}", locations.iter().min().unwrap());
+}
+
+fn extract_seeds_from_ranges(seed_ranges: Vec<i64>) -> Vec<i64> {
+    let seed_starts: Vec<i64> = seed_ranges.clone().into_iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 0)
+        .map(|(_, e)| e)
+        .collect();
+
+    let seed_range_lengths: Vec<i64> = seed_ranges.into_iter()
+        .enumerate()
+        .filter(|&(i, _)| i % 2 == 1)
+        .map(|(_, e)| e)
+        .collect();
+
+    return zip(seed_starts, seed_range_lengths)
+        .flat_map(|(s, l)| (s..s+l).collect::<Vec<i64>>())
+        .collect();
+}
+
+fn extract_seeds(line: String) -> Vec<i64> {
+    let (_, seeds_string) = line.split_once(" ").unwrap();
+
+    let seeds: Vec<i64> = seeds_string
+        .split(" ")
+        .map(String::from)
+        .map(|x| x.parse::<i64>())
+        .map(|x| x.unwrap())
+        .collect();
+
+    return seeds;
+}
+
+fn extract_rules(block: String) -> Vec<Rule> {
+    let (_, rule_strings) = block.split_once("\n").unwrap();
+
+    let rules: Vec<Rule> = rule_strings
+        .split("\n")
+        .filter(|&x| !x.is_empty())
+        .map(String::from)
+        .map(parse_rule)
+        .collect();
+
+    return rules;
+}
+
+fn parse_rule(line: String) -> Rule {
+    let components: Vec<String> = line
+        .split(" ")
+        .map(String::from)
+        .collect();
+
+    return Rule {
+        target_start: components[0].parse::<i64>().unwrap(),
+        source_start: components[1].parse::<i64>().unwrap(),
+        range: components[2].parse::<i64>().unwrap(),
+    };
+}
+
+fn apply_rules(source: i64, rules: Vec<Rule>) -> i64 {
+    let rule = rules.into_iter()
+        .find(|r| r.source_start <= source && source < r.source_start + r.range);
+
+    match rule {
+        Some(r) => return r.target_start + (source - r.source_start),
+        None => return source,
+    }
+}
+
+#[derive(Clone)]
+struct Rule {
+    source_start: i64,
+    target_start: i64,
+    range: i64,
+}
