17559d29d847617f8f86ea87590e0def739364d4
SoullessMindful <soullessmindful@gmail.com>
2024-07-19 08:36:44 +0200

    Day 6: JS: A slight optimization to solution to part two. Time needed is roughly halved.

--- day/6/js/day-6-part-2.js
+++ day/6/js/day-6-part-2.js
@@ -18,14 +18,13 @@ const distance = Number.parseInt(distStr
   .slice(1)
   .join(''))
 
-const possibleDistances = range(1, time - 1)
-  .map((T) => T*(time-T))
+const firstWinningTime = range(1, time - 1)
+  .find((T) => T*(time-T) > distance)
 
-const possibleWinCount = possibleDistances
-  .filter((pd) => pd > distance)
-  .length
+const lastWinningTime = time - firstWinningTime
+const result = lastWinningTime - firstWinningTime + 1
 
-console.log(possibleWinCount)
+console.log(result)
 
 function range(start, length) {
   return Array.from({ length }).map((_, i) => start + i)
